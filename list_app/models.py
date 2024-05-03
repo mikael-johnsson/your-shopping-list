3345ea6f219fba8b9efd388e0f715521054c04e3
Mikael Johnsson <mikaeljohanjohnsson@gmail.com>
2024-05-03 09:02:05 +0000

    create list model and migrate it

--- list_app/models.py
+++ list_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class List(models.Model):
+    #list_id = models.IntegerField()
+    name = models.CharField(max_length=25, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="lists"
+    )
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+    
+    def __str__(self):
+        return f"Name of list: {self.name}"
