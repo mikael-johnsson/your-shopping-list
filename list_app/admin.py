ef220c3b840165ffaa59f0374798a496860650e0
Mikael Johnsson <mikaeljohanjohnsson@gmail.com>
2024-05-03 09:41:57 +0000

    install summernote, create ListAdmin class

--- list_app/admin.py
+++ list_app/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import List, ListItem
 
+@admin.register(List)
+class ListAdmin(SummernoteModelAdmin):
+    list_display = ('name', 'author', 'created_on')
+    search_fields = ['name']
+
 # Register your models here.
-admin.site.register(List)
 admin.site.register(ListItem)
--- your_shopping_list/settings.py
+++ your_shopping_list/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'list_app',
+    'django_summernote',
 ]
 
 MIDDLEWARE = [
