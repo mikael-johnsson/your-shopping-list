273f55e5e6bab467e08f6f34fcb4f62573ca14c6
Mikael Johnsson <mikaeljohanjohnsson@gmail.com>
2024-05-06 13:56:57 +0000

    add variable names to list_detail elements

--- list_app/views.py
+++ list_app/views.py
@@ -8,6 +8,10 @@ class UsersLists(generic.ListView):
     queryset = List.objects.all()
     template_name = "list_app/list_list.html"
 
+class ListItems(generic.ListView):
+    queryset = ListItem.objects.all()
+    template_name = "list_app/list_detail.html"
+
 
 def list_detail(request, name):
     """
@@ -17,11 +21,14 @@ def list_detail(request, name):
 
     queryset = List.objects.all()
     list = get_object_or_404(queryset, name=name)
+    items = ListItem.objects.all()
+    
 
     return render(
         request,
         "list_app/list_detail.html",
-        {"list": list}
+        {"list": list,
+        "items": items}
     )
 
 
