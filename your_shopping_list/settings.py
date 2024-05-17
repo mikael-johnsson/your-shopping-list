4e35bc8f321b8c24326e0862a9054e982ce54b00
Mikael Johnsson <mikaeljohanjohnsson@gmail.com>
2024-05-17 11:27:26 +0000

    update styling on links

--- your_shopping_list/settings.py
+++ your_shopping_list/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-mikaeljohns-yourshoppin-h4rr394r752.ws-eu114.gitpod.io',
+ALLOWED_HOSTS = ['8000-mikaeljohns-yourshoppin-h4rr394r752.ws-eu111.gitpod.io',
                 '.herokuapp.com']
 
 
