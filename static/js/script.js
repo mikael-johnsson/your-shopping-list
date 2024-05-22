6deca3f53260153bc7d84fa957ff4bf6b07a316b
Mikael Johnsson <mikaeljohanjohnsson@gmail.com>
2024-05-22 12:54:01 +0000

    add docstrings to javascript functions

--- static/js/script.js
+++ static/js/script.js
@@ -2,6 +2,11 @@
 
 // Delete list confirmation modal
 const deleteListButton = document.getElementById('delete-list-button')
+/**
+ * Function that displays the Delete List Modal when user clicks 
+ * the delete list button in list_detail.html.
+ * 
+ */
 function showDeleteListModal(){
     const deleteConfirmationModal = document.getElementById("delete-confirmation-modal")
     const modal = new bootstrap.Modal(deleteConfirmationModal);
@@ -15,6 +20,10 @@ if(deleteListButton){
 
 // Share list modal
 const shareListButton = document.getElementById("share-list-button")
+/**
+ * Function that displays the Share List Modal when user clicks 
+ * the Share List button in list_detail.html
+ */
 function shareListModal(){
     const emailModal = document.getElementById("share-list-modal")
     const modal = new bootstrap.Modal(emailModal);
@@ -27,6 +36,11 @@ if(shareListButton){
 }
 
 // Share list function
+/**
+ * Function that lets the user send the list via EmailJS.
+ * Via a form the function gathers the users name, the receivers email, 
+ * a message and the list containing the list items.
+ */
 function sendMail(shareList) {
     let listName = document.getElementById("list-name")
     let listItemElements = document.getElementsByClassName("list-items")
@@ -56,6 +70,10 @@ function sendMail(shareList) {
     
 // Give new list a name modal
 const newListModal = document.getElementById("new-list-modal")
+/**
+ * Function that displays the New List modal when user clicks 
+ * Create New List button.
+ */
 function showNewListModal(){
     const modal = new bootstrap.Modal(newListModal);
     modal.show();
@@ -85,7 +103,10 @@ for (let form of updateForms){
     form.style.display = "none"
 }
 
-//Toggle item buttons
+/**
+ * Function that toggles the displaying of the item edit and delete buttons
+ * when the user clicks the Edit List button.
+ */
 function showItemButtons(){
     for (let button of editButtons){
         if(button.style.display === "none"){
@@ -104,7 +125,10 @@ function showItemButtons(){
     }
 }
 
-//Hide buttons and display update form
+/**
+ * Function that hides the item edit and delete button of a certain item and 
+ * shows the update item form whn user clicks the Edit button.
+ */
 function editItem(event) {
     let button = event.target
     let parent = button.parentElement
@@ -139,6 +163,10 @@ if(editNameForm){
     editNameForm.style.display = "none";
 }
 
+/**
+ * Function that hides the Edit List Name button and displays the 
+ * Update List Name form when user clicks the Edit List Name button.
+ */
 function editListName() {
     editNameButton.style.display = "none";
     editNameForm.style.display = "block";
@@ -151,6 +179,10 @@ if(editNameButton) {
 // Messages
 let closeButton = document.getElementById("message-close")
 
+/**
+ * Function that stops displaying the message when the user clicks 
+ * the Close Message button.
+ */
 function closeMessage(){
     messageDiv = document.getElementById("message-container")
     messageDiv.style.display = "none"
